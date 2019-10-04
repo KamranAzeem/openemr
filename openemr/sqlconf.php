a8f10e3efb24393ab4a94c680285af7ca5c710b5
Kamran Azeem <kaz@praqma.net>
2019-10-04 23:03:48 +0200

    Made adjustments to Dockerfile and docker-compose, so at least the setup starts.

---
+++ openemr/sqlconf.php
@@ -0,0 +1,32 @@
+<?php
+//  OpenEMR
+//  MySQL Config
+
+$host	= 'db';
+$port	= '3306';
+$login	= 'user_openemr';
+$pass	= 'pass_openemr';
+$dbase	= 'db_openemr';
+
+//Added ability to disable
+//utf8 encoding - bm 05-2009
+global $disable_utf8_flag;
+$disable_utf8_flag = false;
+
+$sqlconf = array();
+global $sqlconf;
+$sqlconf["host"]= $host;
+$sqlconf["port"] = $port;
+$sqlconf["login"] = $login;
+$sqlconf["pass"] = $pass;
+$sqlconf["dbase"] = $dbase;
+//////////////////////////
+//////////////////////////
+//////////////////////////
+//////DO NOT TOUCH THIS///
+$config = 1; /////////////
+//////////////////////////
+//////////////////////////
+//////////////////////////
+?>
+
